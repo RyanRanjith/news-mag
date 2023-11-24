3372b8f4f8614c1912d1a3c0edc149ad53aa8a8e
RyanRanjith <kingranjith4@gmail.com>
2023-11-24 05:02:09 -0800

    a

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,18 +10,11 @@ const Navbar = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
+        
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="nav-link" href="#">Features</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
     </div>
   </div>
